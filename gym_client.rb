b98ad7d100878d733ad2aaa0579252a04d47afb9
Matthias Richard <matthias.rms@gmail.com>
2016-11-24 10:17:44 +0100

    cleanup

--- gym_client.rb
+++ gym_client.rb
@@ -19,7 +19,7 @@ class Client
     begin
       j = JSON.parse(resp.body)
     rescue
-       puts "Status code: #{resp.code}"
+      puts "Status code: #{resp.code}"
     end
     return j
   end
@@ -137,25 +137,3 @@ class Client
   end
 end
 
-
-  remote_base = 'http://127.0.0.1:5000'
-  client = Client.new(remote_base)
-
-  # Create environment
-  env_id = 'MountainCar-v0'
-  instance_id = client.env_create(env_id)
-
-  # Check properties
-  all_envs = client.env_list_all
-  action_info = client.env_action_space_info(instance_id)
-
-  obs_info = client.env_observation_space_info(instance_id)
-
-  # Run a single step
-  client.env_monitor_start(instance_id, directory='tmp', force=true)
-  init_obs = client.env_reset(instance_id)
-  arr = client.env_step(instance_id, 1, true)
-  client.env_monitor_close(instance_id)
-
-
-  client.upload(training_dir='tmp')
